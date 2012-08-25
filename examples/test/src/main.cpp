2e9a02f9e61796735d43448acf610bf083dcf3d8
Gaulois94git config --global user.name <serenomickael@gmail.com>
2012-08-25 22:12:53 +0200

    Fix the window declaration

--- examples/test/src/main.cpp
+++ examples/test/src/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	guiml::Window window(sf::VideoMode(800, 600, 32), std::string("teste"), NULL, 60, sf::Color::Black, guiml::Image(), true));
+	guiml::Window window(sf::VideoMode(800, 600, 32), std::string("teste"), NULL, 60, sf::Color::Black, guiml::Image());
 	guiml::Frame frame(&window, sf::FloatRect(0, 0, 400, 300), sf::Color::Blue);
 
 	while(window.isOpen())
