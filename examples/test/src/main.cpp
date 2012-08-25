eafc225e802339a472553bad4dab687b03c889a5
Gaulois94 <serenomickael@gmail.com>
2012-08-25 21:49:20 +0200

    For a test compiling

--- examples/test/src/main.cpp
+++ examples/test/src/main.cpp
@@ -5,9 +5,8 @@
 
 int main()
 {
-	guiml::Window window(sf::VideoMode(800, 600, 32), std::string("teste"), NULL, 60, sf::Color::Black, guiml::Image(NULL, std::string("background.jpg"), true));
+	guiml::Window window(sf::VideoMode(800, 600, 32), std::string("teste"), NULL, 60, sf::Color::Black, guiml::Image(), true));
 	guiml::Frame frame(&window, sf::FloatRect(0, 0, 400, 300), sf::Color::Blue);
-	guiml::Image image(&window, std::string("rectangle.jpg"));
 
 	while(window.isOpen())
 	{
